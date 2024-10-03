3adfe3caa7ea8b22521b1ed1694b473cee546c46
Mohan Raj Senthilnathan <mohanraj.senthilnath@gendigital.com>
2024-10-03 16:13:47 +0530

    country individual codes

---
+++ src/Canada/canadaProvinceCode.ts
@@ -0,0 +1,15 @@
+﻿export const CanadaProvinceCode = {
+    Alberta: "AB",
+    BritishColumbia: "BC",
+    Manitoba: "MB",
+    NewBrunswick: "NB",
+    NewfoundlandAndLabrador: "NL",
+    NorthwestTerritories: "NT",
+    NovaScotia: "NS",
+    Nunavut: "NU",
+    Ontario: "ON",
+    PrinceEdwardIsland: "PE",
+    Quebec: "QC",
+    Saskatchewan: "SK",
+    Yukon: "YT"
+}
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ export { ThreeLetterISORegionCode } from './threeLetterISORegionCode';
 export { CountryCallingCodes } from './countryCallingCodes';
 export { CountryCurrencySymbols } from './countryCurrencySymbols';
 export { CountryCurrencyCodes } from './countryCurrencyCodes';
+export { ChinaProvinceCode } from './China/chinaProvinceCode';
+export { CanadaProvinceCode } from './Canada/canadaProvinceCode';
+export { USAStateCode } from './UnitedStates/USAStateCode';
