d0d1fd9acc1437186f5b9bf0bf4bf988bf66f821
Mohan Raj Senthilnathan <mohanraj.senthilnath@gendigital.com>
2024-10-06 13:22:57 +0530

    run build on each change

---
+++ examples/typescript/index.ts
@@ -0,0 +1,10 @@
+import { CanadaProvinceCode, ChinaProvinceCode, CountryCallingCodes, CountryCurrencyCodes, CountryCurrencySymbols, ThreeLetterISORegionCode, TwoLetterISORegionCode, USAStateCode } from 'country-codes-library';
+
+console.log(`USA's Two Letter Country Code is ${TwoLetterISORegionCode.UnitedStates}`);
+console.log(`India's Three Letter Country Code is ${ThreeLetterISORegionCode.India}`);
+console.log(`Germany's Telephone Code is ${CountryCallingCodes.Germany}`);
+console.log(`Alabama's Two Letter State Code is ${USAStateCode.Alabama}`);
+console.log(`Ontario's Two Letter Province Code is ${CanadaProvinceCode.Ontario}`);
+console.log(`Beijing's Two Letter Province Code is ${ChinaProvinceCode.Beijing}`);
+console.log(`Bangladesh's Currency Code is ${CountryCurrencyCodes.Bangladesh}`);
+console.log(`Japan's Currency Symbol is ${CountryCurrencySymbols.Japan}`);
